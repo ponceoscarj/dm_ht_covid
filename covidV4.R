f9845e43edb7287c97fd2aa2f376e4cd0b258c6d
Oscar Ponce <ponceoscarj@gmail.com>
2020-05-02 23:40:23 -0500

    further

--- covidV4.R
+++ covidV4.R
@@ -22,23 +22,21 @@ options(tibble.width = NULL)
 
 #####Claning Data#####
 getwd()
-#setwd()
-
 list.files()
 
-prev1 <- read_excel("C:/Users/Oscar Ponce/Documents/Research/GitHub/dm_ht_covid/finalV2.xlsx", 
-                    sheet = "Non-exclusive prevalence")
+
+prev1 <- read.csv("C:/Users/Oscar Ponce/Documents/Research/GitHub/dm_ht_covid/databases/proportions.csv")
 
 #General info of included articles
-info <- read.csv("C:/Users/Oscar Ponce/Documents/Research/GitHub/dm_ht_covid/general_info.csv")
+info <- read.csv("C:/Users/Oscar Ponce/Documents/Research/GitHub/dm_ht_covid/databases/general_info.csv")
 
 
 names(info)[1] <- 'id'
 names(info)[3] <- 'author'
 
 
+#General info of included articles
 
-#C:/Users/Oscar Ponce/Documents/Research/Frank/COVID/5 Data analysis/finalV2.xlsx
 
 names(prev1)[names(prev1) == "analyzed_n"] <- "n"
 names(prev1)[names(prev1) == "n1"] <- "events"
@@ -62,9 +60,6 @@ prev1$country[prev1$country == 'republic_of_korea'] <- 'korea'
 prev1$country[is.na(prev1$country)] <- 'hong_kong'
 
 
-
-colnames(prev1)
-
 str(prev1$final_exposure)
 table(prev1$final_exposure)
 prev1$final_exposure[prev1$final_exposure == 
@@ -75,11 +70,11 @@ prev1$final_exposure[prev1$final_exposure ==
                        "hypertension"] <- 'HTN'
 
 prev1$pop1 <- ifelse((prev1$group == "death" & prev1$setting =="overall"),"death",
-                    ifelse((prev1$group == "general" & prev1$setting =="outpatient"), "outpatient",
-                    ifelse((prev1$group == "general" & prev1$setting =="inpatient")| 
-                            (prev1$group == "severe" & prev1$setting =="inpatient") |
-                            (prev1$group == "cardiac" & prev1$setting =="nr") |
-                            (prev1$group == "cardiac" & prev1$setting =="inpatient"), "inpatient", ""))) 
+                     ifelse((prev1$group == "general" & prev1$setting =="outpatient"), "outpatient",
+                            ifelse((prev1$group == "general" & prev1$setting =="inpatient")| 
+                                     (prev1$group == "severe" & prev1$setting =="inpatient") |
+                                     (prev1$group == "cardiac" & prev1$setting =="nr") |
+                                     (prev1$group == "cardiac" & prev1$setting =="inpatient"), "inpatient", ""))) 
 
 
 prev1$pop2 <- ifelse (( prev1$group =="severe"), "severe", "")
@@ -88,9 +83,9 @@ prev1 <- prev1[,-c(17:27)]
 
 
 #Cleaning for effect estimates
-univ <- read_excel("C:/Users/Oscar Ponce/Documents/Research/GitHub/dm_ht_covid/finalV2.xlsx", 
-                   sheet = "Non-exclusive data") 
-#C:/Users/Oscar Ponce/Documents/Research/Frank/COVID/5 Data analysis/finalV2.xlsx
+univ <- read.csv("C:/Users/Oscar Ponce/Documents/Research/GitHub/dm_ht_covid/databases/effectsize.csv")
+
+
 
 table(univ$selection_criteria)
 names(univ)[names(univ) == "selection_criteria"] <- 'group'
@@ -101,10 +96,11 @@ univ$group[univ$group == 'general_influenza'] <- 'general'
 table(univ$group)
 names(univ)[names(univ) == 'id...20']<- 'id'
 
-unimulti <- read_excel("C:/Users/Oscar Ponce/Documents/Research/GitHub/dm_ht_covid/final_unimulti.xlsx",
-                       sheet = "unimulti") 
 
-#C:/Users/Oscar Ponce/Documents/Research/Frank/COVID/5 Data analysis/final_unimulti.xlsx
+
+unimulti <- read.csv("C:/Users/Oscar Ponce/Documents/Research/GitHub/dm_ht_covid/databases/unimulti.csv")
+
+
 
 unimulti <- unimulti[!(unimulti$`Time of measure of exposure`=="Symptom onset"),-8]
 unimulti <- unimulti[!(unimulti$id %in% c(9123, 9171)), ]
@@ -183,11 +179,11 @@ fma1 <- fma1 %>% add_row( .before = 32)
 tfma1 <- cbind( 
   c( "Author", fma1$author, 
      paste("Overall proportion for", ma1$k.eff, "studies","\n", 
-  "(Tau^2 = ", (formatC(ma1$tau2, digits=2, format="f")), ", df = ", (ma1$k - ma1$p),
-     ", p ", (ifelse(ma1$QEp.Wld < 0.001, 
-                    paste("< 0.001"),
-                    paste("= ", formatC(ma1$QEp.Wld, digits=2, format="f")))),
-  "; ", "I^2", " = ", (formatC(ma1$I2, digits=1, format="f")), "%)")),
+           "(Tau^2 = ", (formatC(ma1$tau2, digits=2, format="f")), ", df = ", (ma1$k - ma1$p),
+           ", p ", (ifelse(ma1$QEp.Wld < 0.001, 
+                           paste("< 0.001"),
+                           paste("= ", formatC(ma1$QEp.Wld, digits=2, format="f")))),
+           "; ", "I^2", " = ", (formatC(ma1$I2, digits=1, format="f")), "%)")),
   c( "Frequency (n/N)",fma1$rate, paste(sum(ma1$xi), " / ",sum(ma1$ni))),
   c( "Prevalence (95% CI)", fma1$prop, 
      paste(formatC(expma1$pred, format='f', digits =2), 
@@ -212,7 +208,7 @@ forestplot(tfma1,
            new_page = TRUE,
            colgap = unit(5, "mm"),
            hrzl_lines = list("2" = gpar (lwd=1, columns=c(1:4), col="black") 
-                             ),
+           ),
            lineheight=unit(0.5,'cm'),
            line.margin = 2,
            is.summary = c(T, rep(F, 32), T),
@@ -246,7 +242,7 @@ expma2 <- predict(ma2, transf=transf.ilogit, digits=3)
 
 
 ma3 <- rma.glmm(measure="PLO", xi=events, ni=n, 
-                        subset=(final_exposure=="DM" & pop1 =="inpatient" ), data=prev1)
+                subset=(final_exposure=="DM" & pop1 =="inpatient" ), data=prev1)
 expma3 <- predict(ma3, transf=transf.ilogit, digits=3)
 
 
@@ -733,7 +729,7 @@ par(ask=F)
 
 
 ma12 <- rma(measure="PLO", xi=events, ni=n, 
-                subset=(final_exposure=="DM & HTN" & pop2=="severe"), data=prev1)
+            subset=(final_exposure=="DM & HTN" & pop2=="severe"), data=prev1)
 expma12 <- predict(ma12, transf=transf.ilogit, digits=3)
 
 
@@ -759,8 +755,8 @@ x <- data.frame(rbind(
   c( "Overall",  ma10[[15]], sum(ma10[[55]]), sum(ma10[[61]]), ma10[[28]], expma10[[1]], expma10[[3]], expma10[[4]]), 
   c( "Severe COVID-19",  ma12[[15]], 2, sum(ma12[[50]]), "na", expma12[[1]], expma12[[3]], expma12[[4]]) 
   
-  ), 
-  stringsAsFactors = FALSE) 
+), 
+stringsAsFactors = FALSE) 
 
 
 
@@ -849,172 +845,6 @@ par(ask=F)
 
 help("forestplot")
 
-#####Meta analysis of proportions by country####
-table(prev1$country)
-
-print(prev1[prev1$final_exposure=="DM" ,c(1,2,4,5,11,12,14,15,16,28,29)])
-
-ma1 #overall DM
-expma1 #overall DM
-
-ma13 <- rma(measure="PLO", xi=events, ni=n, subset=(final_exposure=="DM" & country=="austalia"), data=prev1)
-expma13 <- predict(ma13, transf=transf.ilogit, digits=3)
-
-ma14 <- rma.glmm(measure="PLO", xi=events, ni=n, subset=(final_exposure=="DM" & country=="china"), data=prev1)
-expma14 <- predict(ma14, transf=transf.ilogit, digits=3)
-
-ma15 <- rma(measure="PLO", xi=events, ni=n, subset=(final_exposure=="DM" & country=="singapore"), data=prev1)
-expma15 <- predict(ma15, transf=transf.ilogit, digits=3)
-
-ma16 <- rma(measure="PLO", xi=events, ni=n, subset=(final_exposure=="DM" & country=="hong_kong"), data=prev1)
-expma16 <- predict(ma16, transf=transf.ilogit, digits=3)
-
-ma17 <- rma.glmm(measure="PLO", xi=events, ni=n, subset=(final_exposure=="DM" & country=="korea"), data=prev1)
-expma17 <- predict(ma17, transf=transf.ilogit, digits=3) 
-
-ma18 <- rma(measure="PLO", xi=events, ni=n, subset=(final_exposure=="DM" & country=="italy"), data=prev1)
-expma18 <- predict(ma18, transf=transf.ilogit, digits=3)
-
-ma19 <- rma.glmm(measure="PLO", xi=events, ni=n, subset=(final_exposure=="DM" & country=="usa"), data=prev1)
-expma19 <- predict(ma19, transf=transf.ilogit, digits=3)
-
- 
-ma6 # overall HTN
-expma6 # overall HTN
-
-ma20 <- rma(measure="PLO", xi=events, ni=n, subset=(final_exposure=="HTN" & country=="korea"), data=prev1)
-expma20 <- predict(ma20, transf=transf.ilogit, digits=3)
-
-
-ma21 <- rma(measure="PLO", xi=events, ni=n, subset=(final_exposure=="HTN" & country=="singapore"), data=prev1)
-expma21 <- predict(ma21, transf=transf.ilogit, digits=3)
-
-ma22 <- rma(measure="PLO", xi=events, ni=n, subset=(final_exposure=="HTN" & country=="bolivia"), data=prev1)
-expma22 <- predict(ma22, transf=transf.ilogit, digits=3)
-
-ma23 <- rma.glmm(measure="PLO", xi=events, ni=n, subset=(final_exposure=="HTN" & country=="usa"), data=prev1)
-expma23 <- predict(ma23, transf=transf.ilogit, digits=3)
-
-ma24 <- rma(measure="PLO", xi=events, ni=n, subset=(final_exposure=="HTN" & country=="france"), data=prev1)
-expma24 <- predict(ma24, transf=transf.ilogit, digits=3)
-
-ma25 <- rma(measure="PLO", xi=events, ni=n, subset=(final_exposure=="HTN" & country=="hong_kong"), data=prev1)
-expma25 <- predict(ma24, transf=transf.ilogit, digits=3)
-
-ma26 <- rma.glmm(measure="PLO", xi=events, ni=n, subset=(final_exposure=="HTN" & country=="china"), data=prev1)
-expma26 <- predict(ma25, transf=transf.ilogit, digits=3)
-
-ma27 <- rma.glmm(measure="PLO", xi=events, ni=n, subset=(final_exposure=="HTN" & country=="italy"), data=prev1)
-expma27 <- predict(ma26, transf=transf.ilogit, digits=3)
-
-
-
-
-
-print(prev1[prev1$final_exposure=="HTN" & prev1$country=="france",c(1,2,4,5,11,12,14,15,16,28,29)])
-
-
-x <- data.frame(rbind(
-  c( "group", "nstudies", "n", "N", "i2", "pr", "llci", "ulci"),
-  c( "Overall",  ma1[[15]], sum(ma1[[55]]), sum(ma1[[61]]), ma1[[28]], expma1[[1]], expma1[[3]], expma1[[4]]), 
-  c( "Australia",  ma13[[15]], 1, sum(ma13[[50]]), 'na', expma13[[1]], expma13[[3]], expma13[[4]]), 
-  c( "Singapore",  ma15[[15]], 4, sum(ma15[[50]]), 'na', expma15[[1]], expma15[[3]], expma15[[4]]), 
-  c( "China",  ma14[[15]], sum(ma14[[55]]), sum(ma14[[61]]), ma14[[28]], expma14[[1]], expma14[[3]], expma14[[4]]), 
-  c( "Hong Kong",  ma16[[15]], 8, sum(ma16[[50]]), 'na', expma16[[1]], expma16[[3]], expma16[[4]]), 
-  c( "South Korea",  ma17[[15]], sum(ma17[[55]]), sum(ma17[[61]]), ma17[[28]], expma17[[1]], expma17[[3]], expma17[[4]]),
-  c( "Italy",  ma18[[15]], 180, sum(ma18[[50]]), 'na', expma18[[1]], expma18[[3]], expma18[[4]]), 
-  c( "USA",  ma19[[15]], sum(ma19[[55]]), sum(ma19[[61]]), ma19[[28]], expma19[[1]], expma19[[3]], expma19[[4]]),
-  
-  c( "Overall",  ma6[[15]], sum(ma6[[55]]), sum(ma6[[61]]), ma6[[28]], expma6[[1]], expma6[[3]], expma6[[4]]), 
-  c( "South Korea",  ma20[[15]], 0, sum(ma20[[50]]), 'na', expma20[[1]], expma20[[3]], expma20[[4]]), 
-  c( "Singapore",  ma21[[15]], 1, sum(ma21[[50]]), 'na', expma21[[1]], expma21[[3]], expma21[[4]]), 
-  c( "Bolivia",  ma22[[15]], 1, sum(ma22[[50]]), 'na', expma22[[1]], expma22[[3]], expma22[[4]]), 
-  c( "USA",  ma23[[15]], sum(ma23[[55]]), sum(ma23[[61]]), ma23[[28]], expma23[[1]], expma23[[3]], expma23[[4]]), 
-  c( "Italy",  ma27[[15]], sum(ma27[[55]]), sum(ma27[[61]]), ma27[[28]], expma27[[1]], expma27[[3]], expma27[[4]]), 
-  c( "France",  ma24[[15]], 1, sum(ma24[[50]]), 'na', expma24[[1]], expma24[[3]], expma24[[4]]), 
-  c( "Hong-Kong",  ma25[[15]], 13, sum(ma25[[50]]), 'na', expma25[[1]], expma25[[3]], expma25[[4]]), 
-  c( "China",  ma26[[15]], sum(ma26[[55]]), sum(ma26[[61]]), ma26[[28]], expma26[[1]], expma26[[3]], expma26[[4]]) 
-  ),stringsAsFactors = FALSE) 
-
-colnames(x) <- x[1,]
-x <- x[-1, ] 
-x[,2:8]<- sapply((x[,2:8]), as.numeric)
-x <- transform(x, prop=paste(n, N, sep="/"))
-x$ci <- paste("(",formatC(x$llci, format='f', digits =2),"-",formatC(x$ulci, format='f', digits=2),")")
-round2 = function(x, n=0) {scale<-10^n; sign(x)*trunc(abs(x)*scale+0.5)/scale}
-x$pr2 <- formatC( round2(x$pr, n=2), format='f', digits=2)
-
-x$i2 <- round2(x$i2, n=0)
-x$prop <- as.character(x$prop)
-x$i2[c(1, 4, 6, 8, 9, 13, 14, 17)] <- paste(x$i2[c(1, 4, 6, 8, 9, 13, 14, 17)],"%")
-
-x$i2[c(2,3,5,7,10,11,12,15,16)] <- replace_na(x$i2[c(2,3,5,7,10,11,12,15,16)],"na")
-View(x)
-
-x$prci <- ifelse(is.na(x$pr2), NA,paste(x$pr2, " ", x$ci))
-
-
-x <- x %>% add_row(group="Diabetes", .before = 1) 
-x <- x %>% add_row(group="Hypertension", .before = 10) 
-
-
-sub1 <- c(2,11) #overall
-x$group[sub1] <- paste("   ",x$group[sub1]) 
-
-sub2 <- c(3:9,12:19) #countries
-x$group[sub2] <- paste("      ",x$group[sub2]) 
-
-
-
-tabletext <- cbind( 
-  c( "Disease and subgroup", x$group),
-  c( "N of Studies",x$nstudies),
-  c( "Frequency (n/N)",x$prop),
-  c( "I^2", x$i2),
-  c( "Prevalence (95% CI)", x$prci))
-
-
-
-rrs <- structure(list(
-  mean = c(NA,  x$pr),
-  lower = c(NA,  x$llci),
-  upper = c(NA,  x$ulci)),
-  .Names = c("mean", "lower", "upper"),
-  row.names = c(NA, -20L),
-  class = "data.frame")
-
-
-
-trellis.device(device="windows", height = 25, width = 40, color=TRUE)
-forestplot(tabletext,
-           fn.ci_norm = fpDrawDiamondCI,
-           graph.pos = 5,
-           zero = NA,
-           rrs,new_page = TRUE,
-           colgap = unit(5, "mm"),
-           hrzl_lines = list("2" = gpar (lwd=1, columns=c(1:6), col="black"), "3" = gpar (lwd=0.1, columns=c(1:5), col="black")
-                             , "12" = gpar (lwd=0.1, columns=c(1:5), col="black"), "21" = gpar (lwd=1, columns=c(1:6), col="black")),
-           lineheight=unit(0.5,'cm'),
-           line.margin = 2,
-           is.summary = c(T,T,F,F,F,F,F,F,F,F,
-                          T,F,F,F,F,F,F,F,F,F),
-           align = c("l","l"),
-           ci.vertices = TRUE,
-           txt_gp = fpTxtGp(label =gpar (cex=0.8), 
-                            ticks = gpar(cex = 0.8, fontface="bold"),
-                            summary = gpar(cex = 0.8),
-                            xlab = gpar(cex=0.8)),
-           xticks = c(0, 0.1, 0.2, 0.3, 0.4,0.5, 0.6, 0.7),
-           xlog=FALSE,
-           grid = gpar(lty=3, col="gray"),
-           boxsize = unit(0.22, "cm"),
-           lwd.xaxis = 1,
-           lwd.ci = 2.2,
-           graphwidth = unit(8,"cm"),
-           col=fpColors(box="black",line="grey", axes="grey20"))
-par(ask=F)
-
-
 
 
 #####Meta analysis of estimate of effects univariate and multivariate together#####
@@ -1051,10 +881,10 @@ tfma28 <- cbind(
                            paste("= ", formatC(ma28$QEp, digits=3, format="f")))),
            "; ", "I^2", "= ", (formatC(ma28$I2, digits=1, format="f")), "%)")),
   c( "With Diabetes", "(n/N)", paste(prema28$severe1,"/",prema28$n1), 
-                                paste(sum(prema28$severe1, na.rm=TRUE),"/", sum(prema28$n1, na.rm=TRUE))),
+     paste(sum(prema28$severe1, na.rm=TRUE),"/", sum(prema28$n1, na.rm=TRUE))),
   
   c( "Without Diabetes", "(n/N)", paste(prema28$severe2,"/",prema28$n2), 
-                                    paste(sum(prema28$severe2, na.rm=TRUE),"/",sum(prema28$n2, na.rm=TRUE))),
+     paste(sum(prema28$severe2, na.rm=TRUE),"/",sum(prema28$n2, na.rm=TRUE))),
   
   c( "Relative Risk (95% CI)", NA,  prema28$rr, 
      paste(formatC(expma28$pred, format='f', digits =2), 
@@ -1538,7 +1368,7 @@ x <- data.frame(rbind(
   c( "exposure", "outcome", "analysis (eff)", "nstudies", "n", "N", "i2", "eff", "llci", "ulci", "eff2", "ci", "p"),
   
   c(NA, "Severe COVID-19", "Unadjusted (RR)", ma28[[14]], sum(ma28[[42]], ma28[[44]]), sum(ma28[[50]]), ma28[[25]], expma28[[1]], expma28[[3]], expma28[[4]], NA, NA, ma28[[5]]),
-
+  
   c(NA, "ICU admission", "Unadjusted (RR)",  ma29[[14]], sum(ma29[[42]], ma29[[44]]), sum(ma29[[50]]), ma29[[25]], expma29[[1]], expma29[[3]], expma29[[4]], NA, NA, ma29[[5]]),
   
   c(NA, "Mortality", "Unadjusted (RR)", ma30[[14]], sum(ma30[[42]], ma30[[44]]), sum(ma30[[50]]), ma30[[25]], expma30[[1]], expma30[[3]], expma30[[4]], NA, NA, ma30[[5]]),
@@ -1550,9 +1380,9 @@ x <- data.frame(rbind(
   c(NA, NA, "Adjusted (OR)**", 1, "NR", ht1$n, "NA", ht1$eff, ht1$llci, ht1$ulci, NA, NA, ht1$p2),
   
   c(NA, "ICU admission", "Unadjusted (RR)", ma32[[14]], sum(ma32[[42]], ma32[[44]]), sum(ma32[[50]]), ma32[[25]], expma32[[1]], expma32[[3]], expma32[[4]], NA, NA, ma32[[5]]),
-
+  
   c(NA, "Mortality", "Unadjusted (RR)", ma33[[14]], sum(ma33[[42]], ma33[[44]]), sum(ma33[[50]]), ma33[[25]], expma33[[1]], expma33[[3]], expma33[[4]], NA, NA, ma33[[5]]),
-
+  
   c(NA, "Severe COVID-19", "Unadjusted (RR)", ma34[[14]], sum(ma34[[42]], ma34[[44]]), sum(ma34[[50]]), "NA", expma34[[1]], expma34[[3]], expma34[[4]], NA, NA, ma34[[5]]),
   c(NA, NA, "Unadjusted (HR)", 1, "NR", ht2$n, "NA", ht2$eff, ht2$llci, ht2$ulci, NA, NA, ht2$p2)), 
   stringsAsFactors = FALSE)
@@ -1618,7 +1448,7 @@ trellis.device(device="windows", height = 30, width = 40, color=TRUE)
 forestplot(tabletext,
            fn.ci_norm = fpDrawDiamondCI,
            graph.pos = 6,
-          
+           
            rrs,new_page = TRUE,
            colgap = unit(2, "mm"),
            hrzl_lines = list("2" = gpar (lwd=2, columns=c(1:8), col="black"), "4" = gpar (lwd=0.1, columns=c(1:6), col="black")
